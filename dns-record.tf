8a8a907ebe254bc1f7c61d57e4602761dd5d0c58
SaltyDev42 <abortingprivacy@protonmail.com>
2021-10-20 23:19:10 +0200

    Fixed borked AWX

--- dns-record.tf
+++ dns-record.tf
@@ -6,14 +6,6 @@ resource "aws_route53_record" "osxp_awx" {
   records = [aws_instance.osxp_awx.public_ip]
 }
 
-resource "aws_route53_record" "osxp_awx_task" {
-  zone_id = aws_route53_zone.osxp.zone_id
-  ttl     = "60"
-  type    = "CNAME"
-  name    = "awx-task"
-  records = [aws_route53_record.osxp_awx.name]
-}
-
 resource "aws_route53_record" "osxp_bastion" {
   zone_id = aws_route53_zone.osxp.zone_id
   ttl     = "60"
